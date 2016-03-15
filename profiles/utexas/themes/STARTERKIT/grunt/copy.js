b288fd5cca57b31b9935e79323574735a6eaa1bc
Joe Goodman <jgoodman@austin.utexas.edu>
2016-03-15 15:57:52 -0500

    Updating starterkit theme.

--- profiles/utexas/themes/STARTERKIT/grunt/copy.js
+++ profiles/utexas/themes/STARTERKIT/grunt/copy.js
@@ -13,21 +13,14 @@ module.exports = {
           {
             expand: true,
             cwd: '<%= path.dist %>/css/',
-            src: ['overrides.css', 'foundation.accordion.css', 'foundation.alert.css', 'foundation.clearing.css',
-            'foundation.dropdown.css', 'foundation.interchange.css', 'foundation.joyride.css', 'foundation.offcanvas.css',
-            'foundation.orbit.css', 'foundation.reveal.css', 'foundation.slider.css', 'foundation.tooltip.css',
-            'foundation.topbar.css'],
+            src: ['overrides.css'],
             dest: 'css'
           },
           // Scripts
           {
             expand: true,
             cwd: '<%= path.dist %>/js/',
-            src: ['foundation.accordion.js', 'foundation.alert.js', 'foundation.clearing.js',
-            'foundation.dropdown.js', 'foundation.abide.js', 'foundation.interchange.js',
-            'foundation.joyride.js', 'foundation.offcanvas.js', 'foundation.orbit.js',
-            'foundation.reveal.js', 'foundation.slider.js', 'foundation.tooltip.js',
-            'foundation.topbar.js'],
+            src: [],
             dest: 'js'
           }
         ]
--- profiles/utexas/themes/STARTERKIT/template.php
+++ profiles/utexas/themes/STARTERKIT/template.php
@@ -3,32 +3,3 @@
  * @file
  * Main template file for subtheme of forty_acres theme.
  */
-
-/**
- * Preprocessor for page.tpl.php.
- *
- * Adds in new stylesheets for base, core home,
- * core pages. Also handles adding new javascript, and setting page theme
- * suggestions based on the field_template value. It also throws the menus into
- * a variable on the $page array, and defines the partials directory.
- *
- * Finally, it associates the 404 and 403 pages with their theme suggestions.
- */
-function STARTERKIT_preprocess_page(&$vars, $hook) {
-  $vars['partials_dir'] = __DIR__ . '/templates/partials/';
-
-  // Load foundation js files selected in theme settings.
-  $path = drupal_get_path('theme', 'STARTERKIT');
-  $extra_libraries = theme_get_setting('foundation_files') ? theme_get_setting('foundation_files') : array();
-  foreach ($extra_libraries as $library => $value) {
-    if ($library === $value) {
-      drupal_add_js($path . '/js/foundation.' . $library . '.js', array(
-        'scope' => 'foot_scripts',
-        'weight' => 4,
-      ));
-      if (file_exists($path . '/css/foundation.' . $library . '.css')) {
-        drupal_add_css($path . '/css/foundation.' . $library . '.css');
-      }
-    }
-  }
-}
