c4e56f67e5136a69551e45ca724256ee52bf9d5d
markfullmer <mfullmer@gmail.com>
2016-08-21 23:22:21 -0500

    Initial commit of migration demo

---
+++ sites/all/modules/custom/mymodule/example.settings.php
@@ -0,0 +1,21 @@
+<?php
+
+$databases['legacy']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'moody_main',
+  'username' => '',
+  'password' => '',
+  'host' => 'localhost',
+  'prefix' => '',
+  'collation' => 'utf8_general_ci',
+);
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'bootcamp',
+  'username' => '',
+  'password' => '',
+  'host' => 'localhost',
+  'prefix' => '',
+  'collation' => 'utf8_general_ci',
+);
